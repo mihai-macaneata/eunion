c2eb0157d5c81afbbde07351e14c5bfe56bc6aed
mihai-macaneata <themacaneata@gmail.com>
2019-06-14 00:21:25 +0300

    even better animation

--- src/components/core/Menu.vue
+++ src/components/core/Menu.vue
@@ -495,7 +495,7 @@ import { setTimeout } from 'timers';
   transition: all .3s ease;
 }
 .slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
 .slide-fade-enter, .slide-fade-leave-to {
   transform: translateX(-10px);
